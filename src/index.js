8ef9869d54b1d2a2e1bda4a67e29c871851b9866
code-rmc <rodrigoslmatias@gmail.com>
2022-03-20 18:46:03 -0300

    add handler router

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import App from './App';
 import store from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import AppRoutes from './routes';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <AppRoutes>
+        <App />
+      </AppRoutes>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
