19db1f3d07030940aea360bcc4ba23418ea59e13
Joniruiz <ruiz.jonathan2018@gmail.com>
2022-03-20 22:46:49 -0300

    add setup Loader with the react spinner library

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
