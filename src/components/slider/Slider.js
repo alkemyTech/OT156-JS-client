9a7da3c2c2d05fa26e18d303a437603f865d3275
Adrián Rodíguez <Appxims@gmail.com>
2022-03-23 23:06:05 -0300

    improvements in indentation - and cleaning of all comments

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,18 +1,14 @@
 import React, { useEffect, useState } from 'react'
-import './slider.css';
-import { dataTest } from "./dataSlider";
 import { FaArrowAltCircleLeft,FaArrowAltCircleRight } from 'react-icons/fa'
-/*
-    fix slide
-*/
+
+import { dataTest } from "./dataSlider";
+import './slider.css';
+
 const Slider = () => {
-    // data slide
-    const [ data, setData ]=useState([]);
-    //current image selector
-    const [current,setCurrent]= useState(0);
+    const [ data, setData ] = useState([]);
+    const [current,setCurrent] = useState(0);
     const delay = 5000;
-  
- 
+
     useEffect(() => {
         setData(dataTest);
         }, []) 
@@ -31,22 +27,22 @@ const Slider = () => {
     if (!Array.isArray(data) || data.length <=0){
         return null;
     };
-  return (
-    <section className='slider'>
-        <FaArrowAltCircleLeft className='izquierda-arrow' onClick={prevSlide}/>
-        <FaArrowAltCircleRight className='derecha-arrow' onClick={nextSlide}/>
-        {
-            data?.map( (slide,index)=>{
-                return (                    
-                        <div className={ index === current ? 'slide active' : 'slide'}  key={index} >
-                            {index === current && <div>
-                                 <img src={slide.imageUrl } alt={slide.text} className='image' />
-                                 <h3>{slide.text}</h3>
-                                </div>   }  
-                        </div>)
-        })}
-    </section>
-  )
+    return (
+        <section className='slider'>
+            <FaArrowAltCircleLeft className='left-arrow' onClick={prevSlide}/>
+            <FaArrowAltCircleRight className='right-arrow' onClick={nextSlide}/>
+            {
+                data?.map( (slide,index)=>{
+                    return (                    
+                            <div className={ index === current ? 'slide active' : 'slide'}  key={index} >
+                                {index === current && <div>
+                                    <img src={slide.imageUrl } alt={slide.text} className='image' />
+                                    <h3>{slide.text}</h3>
+                                    </div>   }  
+                            </div>)
+            })}
+        </section>
+    )
 }
 
 export default Slider
