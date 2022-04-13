ed5c12c3cf99c8b17f4905e447f18138e403f8ff
Adrián Rodíguez <Appxims@gmail.com>
2022-04-13 19:24:48 -0300

    add footer component

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,21 +1,58 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
+  const [info, setInfo] = useState({});
+
+
+  //const [result, setResult] = useState({});
+
+  const getData = async () => {
+    const res = await axios.get('http://localhost:3000/organizations/1/public');
+    const data = await res.data;
+    setInfo(data);
+  };
+
+  useEffect(() => {
+    // url de info de organizacion    
+    getData();
+    return () => {
+      // guardo los datos en el state
+    }
+  }, [])
+  
   return (
-    <div className='container' >
-        <div className='wrapContainer'>
-            <div className='logo'>
-
+    <footer className='container bg-light ' >
+        <hr />
+        <section className='row '>
+          <div className='link d-flex justify-content-around '>
+            <section className='left-section d-flex justify-content-start '>
+              <Link className='p-3 text-decoration-none' to={"/news"}>Noticias</Link>
+              <Link className='p-3 text-decoration-none' to={"/news"}>Actividades</Link>
+              <Link className='p-3 text-decoration-none' to={"/news"}>Novedades</Link>
+            </section>
+            <section className='logoSection d-flex justify-content-center'>
+              <img src={info?.image} alt={info?.name} width="40px" height="40px"/>
+            </section>
+            <section className='rigth-section d-flex justify-content-end'>
+              <div className='d-flex justify-content-around'> 
+                <Link className='p-3 text-decoration-none' to={"/news"}>Testimonio</Link>
+                <Link className='p-3 text-decoration-none' to={"/news"}>Nosotros</Link>
+              </div>
+              
+            </section>
+          </div>
+          <hr />
+            <div className='social-media justify-content-center'>
+              
             </div>
-            <div className='linksWeb'>
+            <section className='disclamer d-flex justify-content-center'>
+              2021 by Alkemy. All Rights Reserved.
+            </section>
 
-            </div>
-            <div className='socialMedia'>
-
-            </div>
-
-        </div>
-    </div>
+        </section>
+    </footer>
   )
 }
 
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Footer from "../../components/footer/Footer";
+import Header from "../../components/Header";
 import Home from '../../components/home/Home';
 
 const HomePage = () => {
   return (<> 
+  <Header />
   <Home welcome={"Bienvenidos"}/>
   <Footer />
   </>)
