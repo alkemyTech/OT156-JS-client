fdf38d7a5b59e85de0173dc64999cae424413e8d
Adrián Rodíguez <Appxims@gmail.com>
2022-04-13 11:00:46 -0300

    create footer component

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='container' >
+        <div className='wrapContainer'>
+            <div className='logo'>
+
+            </div>
+            <div className='linksWeb'>
+
+            </div>
+            <div className='socialMedia'>
+
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Footer
