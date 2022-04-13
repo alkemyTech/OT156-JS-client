7011399463ee86655c3fe4a939468935473bd3b1
Adrián Rodíguez <Appxims@gmail.com>
2022-04-13 11:01:38 -0300

    fix home component

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Footer from "../../components/footer/Footer";
+import Home from '../../components/home/Home';
 
-const Home = () => {
-  return (<h1>Home</h1>)
+const HomePage = () => {
+  return (<> 
+  <Home welcome={"Bienvenidos"}/>
+  <Footer />
+  </>)
 };
 
-export default Home;
+export default HomePage;
