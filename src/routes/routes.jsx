a4d768d9be3a5e18c28b9d1bc318bc1eeadd27a3
Nazareno del Rìo <nazareno7870@gmail.com>
2022-04-15 10:39:35 -0300

    Automatically authenticate by token (#21)

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useDispatch } from 'react-redux';
+import { login } from '../features/user/userSlice';
+import { getUserByToken } from '../services/authByToken';
 
 import Header from '../components/Header';
 import Home from '../pages/home/Home';
@@ -9,6 +12,17 @@ import Category from '../pages/category';
 import BackOffice from './../pages/backoffice/index';
 
 const AppRoutes = () => {
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    getUserByToken().then(res => {
+      dispatch(login(res));
+    })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <BrowserRouter>
       <Header />
