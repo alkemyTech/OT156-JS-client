52b642bd1c2fb74902693c2c2bb26308cc5fe285
Adrián Rodíguez <Appxims@gmail.com>
2022-04-16 18:55:18 -0300

    fix aligment

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -24,17 +24,17 @@ const Footer = () => {
   return (
     <footer className='container bg-light ' >
         <hr />
-        <section className=''>
+        <section className='row'>
           <div className='link d-flex justify-content-around '>
-            <section className=' d-flex left-section align-items-center '>
+            <section className=' d-flex col-sm justify-content-around '>
               <Link className='p-3 text-decoration-none' to={"/news"}>Noticias</Link>
               <Link className='p-3 text-decoration-none' to={"/news"}>Actividades</Link>
               <Link className='p-3 text-decoration-none' to={"/news"}>Novedades</Link>
             </section>
-            <section className='logoSection'>
+            <section className='col-sm-2 d-flex justify-content-center logoSection '>
               <img src={info?.image} alt={info?.name} width="40px" height="40px"/>
             </section>
-            <section className='rigth-section '>
+            <section className='d-flex col-sm row justify-content-between '>
               <div className='d-flex justify-content-evenly'> 
                 <Link className='p-3 text-decoration-none' to={"/news"}>Testimonio</Link>
                 <Link className='p-3 text-decoration-none' to={"/news"}>Nosotros</Link>
@@ -43,10 +43,10 @@ const Footer = () => {
             </section>
           </div>
           <hr />
-            <div className='social-media d-flex justify-content-center mr-3'>
-            <AiOutlineFacebook size={30}/> <AiOutlineInstagram size={30}/> <FiTwitter size={30}/>
+            <div className='social-media d-flex justify-content-center m-3 px-2 mb-3'>
+              <a href='#'><AiOutlineFacebook size={30}/></a> <a href='#'><AiOutlineInstagram size={30}/></a> <a href='#'> <FiTwitter size={30}/> </a>
             </div>
-            <section className='disclamer d-flex justify-content-center'>
+            <section className='disclamer d-flex justify-content-center mb-3'>
               2021 by Alkemy. All Rights Reserved.
             </section>
 
