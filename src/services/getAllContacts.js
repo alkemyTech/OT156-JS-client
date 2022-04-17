4aa234390f98500ab949d531e378643b52e4eb24
Adrián Rodíguez <Appxims@gmail.com>
2022-04-17 19:45:52 -0300

    add service/getAllContacts

---
+++ src/services/getAllContacts.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+const GetAllContacts = () => {
+    const [contacts, setContacts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [refresh, setRefresh] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const result = await axios.get("http://localhost:3000/contacts");
+                setContacts(result.data.entries);
+            } catch (error) {
+                setError(error);
+            }
+            setLoading(false);
+            setRefresh(false);
+        };
+        fetchData();
+    }, [refresh]);
+
+    return { news, loading, error,setRefresh };
+}
+
+export default GetAllContacts;
