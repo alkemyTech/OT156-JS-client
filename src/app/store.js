fa4138d07b14e391326629fc7efc3823e1542679
Nazareno del Rìo <nazareno7870@gmail.com>
2022-04-21 07:18:49 -0300

    Ot156 133 setup redux persist (#31)
    * Create Screen of public News
    
    * add link router in cardnews
    
    * add redux-persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,30 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/user/userSlice";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-export default configureStore({
-  reducer: {
-    user: userReducer
-  },
-});
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, userReducer)
+
+export const store = configureStore({
+  reducer: {user:persistedReducer},
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
--- src/services/authByToken.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export async function getUserByToken() {
-  const token = window.localStorage.getItem("token");
-  let response = await axios.get('http://localhost:3000/auth/me', {
-    headers: { Authorization: `${token}` },
-  });
-  return response.data;
-}
