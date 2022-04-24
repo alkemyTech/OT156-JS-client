c00d8deed76a766c409770824026455940ef4fab
Nazareno <nazareno7870@gmail.com>
2022-04-24 15:44:49 -0300

    Form is added to create categories

--- src/pages/backoffice/categories.jsx
+++ src/pages/backoffice/categories.jsx
@@ -4,10 +4,13 @@ import { Table } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import SuccessAlert from '../../components/Alerts/SuccessAlert';
 import { deleteCategory, GetAllCategories } from '../../services/categories';
+import CategoriesForm from '../../components/CategoriesForm/CategoriesForm';
 
 const CategoriesBackOffice = () => {
     const { categories, setRefresh } = GetAllCategories();
     const [id, setId] = useState(null);
+    const [showCreate, setShowCreate] = useState(false);
+    const [showTable, setShowTable] = useState(true);
     const [showModalDelete, setShowModalDelete] = useState(false);
     const [sendComplete, setSendComplete] = useState({
         error: false,
@@ -54,46 +57,90 @@ const CategoriesBackOffice = () => {
         });
 
     }
+
+    const handleNewCategory = () => {
+        setShowCreate(true);
+        setShowTable(false);
+    }
+
+    const handleCreate = () => {
+        setSendComplete({
+            error: false,
+            success: true,
+            msg: 'Categoria Creada Correctamente.',
+        });
+        setShowCreate(false);
+        setShowTable(true);
+        setRefresh(true);
+        setTimeout(() => {
+            setSendComplete({
+                error: false,
+                success: false,
+                msg: 'Categoria Creada Correctamente.',
+            });
+        }, 2000);
+    }
+
+    const handleBack = () => {
+        setShowCreate(false);
+        setShowTable(true);
+        setId(null);
+        setRefresh(true);
+        setShowModalDelete(false);
+    }
+
+
     return (
         <div className="tableContainer">
             <div className="backButton">
                 <div>
                     <Link to='/backoffice'>🏠 Backoffice</Link>
                 </div>
+                {
+                    showCreate
+                        ? <button className='cancelBtn' onClick={handleBack}>✖ Cancelar</button>
+                        : <button onClick={handleNewCategory}>📰 Crear Categoría</button>
+                }
             </div>
             <h3>Categorias</h3>
-
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Categoria</th>
-                        <th>Eliminar</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        categories.map(category => {
-                            return (
-                                <tr key={category.id}>
-                                    <td>{category.id}</td>
-                                    <td>{category.name}</td>
-                                    <td>
-                                        <button
-                                            onClick={() => handleShowModalDelete(category.id)}
-                                            className="btn btn-danger">Delete</button>
-                                    </td>
-                                </tr>
-                            )
-                        })
-                    }
-                </tbody>
-            </Table>
+            {
+                showTable &&
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Categoria</th>
+                            <th>Eliminar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            categories.map(category => {
+                                return (
+                                    <tr key={category.id}>
+                                        <td>{category.id}</td>
+                                        <td>{category.name}</td>
+                                        <td>
+                                            <button
+                                                onClick={() => handleShowModalDelete(category.id)}
+                                                className="btn btn-danger">Delete</button>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </Table>}
             {sendComplete.success && (
                 <SuccessAlert title={sendComplete.msg}>
 
                 </SuccessAlert>
             )}
+            {showCreate &&
+                <CategoriesForm
+                    handleCreate={handleCreate}
+                />
+            }
             {showModalDelete && <div className="modalDelete">
                 <div className="modalDelete__container">
                     <h3>Está seguro de eliminar la Categoria?</h3>
