1bd220451ca65c2562d89d217a36a3612640a4f3
AdrianInsomnio <appxims@gmail.com>
2022-04-26 09:50:11 -0300

    fix responsive header- relocate dinamic rol menu (#47)

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,6 +4,9 @@ import axios from 'axios';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { userState } from '../../features/user/userSlice';
+import AdminMenu from '../Menu/AdminMenu';
+import UserMenu from '../Menu/UserMenu';
+import { useDispatch } from 'react-redux';
 
 const Header = () => {
   const [result, setResult] = useState({});
@@ -30,22 +33,33 @@ const Header = () => {
 
   const [userLogged, setUserLogged] =useState(true); 
 
+  const UserInfo = () => {
+    const user = useSelector(userState);
+    const dispatch = useDispatch();
+    return (<>{
+                user.token !== null ? ( user.roleId ===1 ? <AdminMenu />: <UserMenu />):''
+              }
+    </>
+    );
+  }
+
   const MenuLogin =()=>{
     return(
-      <><Nav.Link className="block-example rounded mb-0 border border-primary" href="/login">
+      <><Nav.Link className="block-example rounded mb-0 border border-primary m-2 text-center" href="/login">
       <Link style={{ textDecoration: "none" }} to="/login">Log In</Link>
     </Nav.Link>
-    <Nav.Link className="block-example rounded mb-0 border border-primary bg-primary mx-2" href="/register">
-      <Link style={{ textDecoration: "none", color: "white" }} to="/register">Registrate</Link>
+    <Nav.Link className="block-example rounded mb-0 border border-primary bg-primary m-2 text-center" href="/register">
+      <Link style={{ textDecoration: "none", color: "white" , textalign: "center" }} to="/register">Registrate</Link>
     </Nav.Link>
       </>
     )
   }
 
   return (
-    <Navbar sticky="top">
-      <Container>
-        <Navbar.Brand >
+    <Navbar collapseOnSelect expand="lg" sticky="top">
+      <Container >
+        
+        <Navbar.Brand className="me-auto" >
           <img
             alt={result.name}
             src={result.image}
@@ -55,17 +69,22 @@ const Header = () => {
           />{' '}
           {result.name}
         </Navbar.Brand>
-        <Nav className="mx-auto">
-          {result?.navLink?.map((nav, index) => (
-            <Nav.Link key={index} href={nav.link} >
-              <Link style={{ textDecoration: "none",  }} to={`${nav.link}`}>{nav.name}</Link>
-            </Nav.Link>
-          ))}
-        </Nav>
-        <Nav className="">
-          { !userLogged ? <MenuLogin /> : '' }
-        </Nav>
+        <Navbar.Toggle  aria-controls="responsive-navbar-nav"/>        
+        <Navbar.Collapse id="responsive-navbar-nav" >
+          <Nav  className="me-auto">
+            {result?.navLink?.map((nav, index) => (
+              <Nav.Link key={index} href={nav.link} >
+                <Link style={{ textDecoration: "none",  }} to={`${nav.link}`}>{nav.name}</Link>
+              </Nav.Link>
+            ))}
+          </Nav>
+          <Nav className="me-auto"> 
+          { !userLogged ? <MenuLogin /> : <UserInfo /> }
+        </Nav> 
+        </Navbar.Collapse>       
+
       </Container>
+
     </Navbar>
   );
 };
